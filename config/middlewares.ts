8c8bda5a183e5ccde71908cd71cbe742ab2d5c6e
deepakvishwakarma-hh <79501955+deepakvishwakarma-hh@users.noreply.github.com>
2024-05-20 15:08:10 +0530

    AWS connection setuped

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -9,4 +9,30 @@ export default [
   'strapi::session',
   'strapi::favicon',
   'strapi::public',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:'],
+          'img-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            'market-assets.strapi.io',
+            'ellemora.s3.yourRegion.amazonaws.com',
+          ],
+          'media-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            'market-assets.strapi.io',
+            'ellemora.s3.yourRegion.amazonaws.com',
+          ],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
 ];
--- config/plugins.ts
+++ config/plugins.ts
@@ -1,23 +1,53 @@
-export default () => ({
-    transformer: {
-        enabled: true,
-        config: {
-          responseTransforms: {
-            removeAttributesKey: true,
-            removeDataKey: true,
-          },
-          requestTransforms: {
-            wrapBodyWithDataKey: true,
-          },
-          hooks: {
-            preResponseTransform: (ctx) => {},
-            postResponseTransform: (ctx) => {},
-          },
-          plugins: {
-            ids: {
-              slugify: true,
-            },
+
+
+export default ({ env }) => ({
+  transformer: {
+    enabled: true,
+    config: {
+      responseTransforms: {
+        removeAttributesKey: true,
+        removeDataKey: true,
+      },
+      requestTransforms: {
+        wrapBodyWithDataKey: true,
+      },
+      hooks: {
+        preResponseTransform: (ctx) => { },
+        postResponseTransform: (ctx) => { },
+      },
+      plugins: {
+        ids: {
+          slugify: true,
+        },
+      },
+    },
+  },
+  upload: {
+    config: {
+      provider: "aws-s3",
+      providerOptions: {
+        baseUrl: "https://s3.eu-north-1.amazonaws.com/ellemora",
+        rootPath: "ellemora",
+        s3Options: {
+          accessKeyId: env("AWS_ACCESS_KEY_ID"),
+          secretAccessKey: env("AWS_ACCESS_SECRET"),
+          region: env("AWS_REGION"),
+          params: {
+            // ACL: env("AWS_ACL", "public-read"),
+            signedUrlExpires: env("AWS_SIGNED_URL_EXPIRES", 15 * 60),
+            Bucket: env("AWS_BUCKET"),
           },
         },
       },
+      actionOptions: {
+        upload: {
+          ACL: null
+        },
+        uploadStream: {
+          ACL: null
+        },
+        delete: {},
+      },
+    },
+  },
 });
