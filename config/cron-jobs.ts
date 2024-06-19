eec76722c4b029b13623ad0eccf37b19a86e9664
deepakvishwakarma-hh <79501955+deepakvishwakarma-hh@users.noreply.github.com>
2024-06-19 05:49:13 +0530

    remoev code

--- config/cron-jobs.ts
+++ config/cron-jobs.ts
@@ -5,28 +5,30 @@ const delay = (ms: number): Promise<void> => {
 };
 
 const processCartItemsInBatches = async (
-    userMobileNumbers: any[],
+    notifications: {
+        mobile: string,
+        cartId: number
+    }[],
     batchSize: number,
     delayTime: number
 ): Promise<void> => {
-    for (let i = 0; i < userMobileNumbers.length; i += batchSize) {
-        const batch = userMobileNumbers.slice(i, i + batchSize);
+    for (let i = 0; i < notifications.length; i += batchSize) {
+        const batch = notifications.slice(i, i + batchSize);
 
-        await Promise.all(batch.map(async (mobileNumber) => {
+        await Promise.all(batch.map(async (notification) => {
             // Simulating an API request to process the cart item
-            console.log(`Processing cart item with Mo: ${mobileNumber}`);
-            await sendEllemoraWelcome({ mobile: mobileNumber });
-            console.log(`Processed cart item with Mo: ${mobileNumber}`);
+            console.log(`Processing cart item with Mo: ${notification?.mobile}`);
+            await sendEllemoraWelcome(notification);
+            console.log(`Processed cart item with Mo: ${notification?.mobile}`);
         }));
 
-        if (i + batchSize < userMobileNumbers.length) {
+        if (i + batchSize < notifications.length) {
             await delay(delayTime);
         }
     }
 };
 
-
-export const sendEllemoraWelcome = async ({ mobile }: { mobile: string }) => {
+export const sendEllemoraWelcome = async ({ mobile, cartId }: { mobile: string, cartId: number }) => {
     await fetch("https://graph.facebook.com/v19.0/277625598767650/messages", {
         method: "POST",
         headers: {
@@ -35,17 +37,22 @@ export const sendEllemoraWelcome = async ({ mobile }: { mobile: string }) => {
                 "Bearer EAANZAExOOidkBO82ihrLai5SafMn0qlZCj8O9AyfOFPpSozRDYZCIh1yLT0Sfs4DzlaCS17V2R826JFOAvUYerh1ChwlH1sceSBmmoqGsbrhHmwpZBZABrDCc8CnaZBbqNNhMbjIZADvwzpfIxc03cM46SoFZAFgGfhk8KHxXZBk4kiadqJtwnfkAt7mwA3IXcTD4",
         },
         body: JSON.stringify({
-            messaging_product: "whatsapp",
-            recipient_type: "individual",
             to: `${mobile}`,
             type: "template",
+            messaging_product: "whatsapp",
+            recipient_type: "individual",
             template: {
-                name: "welcome_ellemora",
+                name: "abundant_cart",
                 language: {
                     code: "en",
                 },
             },
         }),
+    });
+    // Update the cart item to be notified
+    await strapi.db.query('api::cart.cart').update({
+        where: { id: cartId },
+        data: { isNotified: true }
     });
 };
 
@@ -53,33 +60,42 @@ export default {
     /** 
      * Simple examples.
      */
-    '* * 0 * * *': async ({ strapi }: { strapi: Strapi }) => {
-        console.log("🚀 ~ file: cron.js ~ executing action ~Every 12AM");
+    // '*/15 * * * * *': async ({ strapi }: { strapi: Strapi }) => {
+    //     console.log("🚀 ~ file: cron.js ~ executing action ~Every 12AM");
+    //     // Fetch all cart items that have not been notified and were created more than 1 hour ago
+    //     const cartItems = await strapi.db.query('api::cart.cart').findMany({
+    //         where: {
+    //             isNotified: {
+    //                 $ne: true
+    //             },
+    //             createdAt: {
+    //                 $lt: new Date(new Date().getTime() - 0 * 0 * 1000)
+    //             }
+    //         },
+    //         populate: ["user"]
+    //     })
 
-        const cartItems = await strapi.db.query('api::cart.cart').findMany({
-            where: {
-                createdAt: {
-                    $lt: new Date(new Date().getTime() - 60 * 60 * 1000)
-                }
-            },
-            populate: ["user"]
-        })
+    //     console.log("cartItems", cartItems)
 
-        const userForNotification = [];
+    //     const userForNotification: {
+    //         mobile: string,
+    //         cartId: number
+    //     }[] = [];
 
-        for (const cartItem of cartItems) {
-            if (cartItem?.user?.mobileNumber === undefined) return;
-            if (userForNotification.includes(cartItem?.user?.mobileNumber)) continue;
-            userForNotification.push(cartItem?.user?.mobileNumber)
-        }
-        console.log("userForNotification", userForNotification)
-        // Process cart items in batches
-        const batchSize = 5;
-        const delayTime = 5000; // 2 seconds delay between batches
-
-        await processCartItemsInBatches(userForNotification, batchSize, delayTime);
-
-    },
+    //     for (const cartItem of cartItems) {
+    //         if (cartItem?.user?.mobileNumber === undefined) return;
+    //         if (userForNotification.find((user) => user.mobile === cartItem?.user?.mobileNumber)) continue;
+    //         userForNotification.push({
+    //             mobile: cartItem?.user?.mobileNumber,
+    //             cartId: cartItem.id
+    //         })
+    //     }
+    //     console.log("userForNotification", userForNotification)
+    //     // Process cart items in batches
+    //     const batchSize = 5;
+    //     const delayTime = 5000; // 2 seconds delay between batches
+    //     await processCartItemsInBatches(userForNotification, batchSize, delayTime);
+    // },
     // '*/10 * * * * *': () => {
     //     console.log("🚀 ~ file: cron.js ~ executing action ~Every 10sec");
     // },
